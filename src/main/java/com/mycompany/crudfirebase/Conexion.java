d83eb56bc43d93ac9e96debd4d942f06d5a4832a
DanielMurciaPerdomo <danielmurciap@gmail.com>
2023-11-10 22:17:22 -0500

    Creacion y conexion a firebase

---
+++ src/main/java/com/mycompany/crudfirebase/Conexion.java
@@ -0,0 +1,36 @@
+
+package com.mycompany.crudfirebase;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+/**
+ *
+ * @author Daniel Murcia
+ */
+public class Conexion {
+    
+    public static Firestore bd;
+    
+    public static void conectar(){
+        try {
+            FileInputStream refreshToken = new FileInputStream("src/veterinaria.json");
+
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(refreshToken))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+            bd = FirestoreClient.getFirestore();
+            System.out.println("Se conecto con exito");
+        } catch (IOException e) {
+            System.err.print("Error "+e.getMessage());
+        }
+
+    }
+}
